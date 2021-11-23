e4271f650348ee5513f6aca7ef481647d2805493
simon <msb.duck@gmail.com>
2021-11-23 21:20:44 +0100

    he añadido un commit de la carpeta alimentos2020 para msb.duck 23-11-2021 21:20

---
+++ alimentos2020/alimentos2020/app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "mysimonparbasedatos";
+     static public $mvc_bd_usuario  = "simonsql";
+     static public $mvc_bd_clave    = "Msb.93345900";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
